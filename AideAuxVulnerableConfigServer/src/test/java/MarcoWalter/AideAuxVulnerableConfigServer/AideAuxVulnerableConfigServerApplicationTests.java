7899244a272c86e576c162dec97e696ee0b941a3
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2024-01-07 13:20:45 +0100

    Ajout du Serveur de Configuration

---
+++ AideAuxVulnerableConfigServer/src/test/java/MarcoWalter/AideAuxVulnerableConfigServer/AideAuxVulnerableConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package MarcoWalter.AideAuxVulnerableConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AideAuxVulnerableConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
